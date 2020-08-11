91dc6f2ff1840ba7de8eec27efd156ccb21d6bcb
deoooo <864451000@qq.com>
2020-08-11 21:51:06 +0800

    完善参数校验

--- models/response/response.go
+++ models/response/response.go
@@ -2,6 +2,7 @@ package response
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
@@ -15,7 +16,14 @@ func Result(code int, data interface{}, msg string, c *gin.Context) {
 }
 
 func Err(err error, c *gin.Context) {
-	Result(http.StatusBadRequest, nil, err.Error(), c)
+	msg := ""
+	switch err.(type) {
+	case validator.ValidationErrors:
+		msg = Translate(err.(validator.ValidationErrors))
+	default:
+		msg = err.Error()
+	}
+	Result(http.StatusBadRequest, nil, msg, c)
 }
 
 func OK(data interface{}, c *gin.Context) {
